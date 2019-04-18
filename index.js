f345e06814a1ccea9dd1ef77a3668748e788b4e5
Eduard Generalov <eduard@generalov.net>
2019-04-18 17:19:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function () {
-  console.log("Listening on port 3000");
+app.listen(80, function () {
+  console.log("Listening on port 80");
 });
